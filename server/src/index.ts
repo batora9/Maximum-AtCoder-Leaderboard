0fd472a8bc3029bfcbe9aa76330ff57b21ccbb2b
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-10-30 01:05:49 +0900

    キャッシュできるようにする

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: Env;
+}>();
 
 app.use(cors());
 
@@ -13,9 +15,21 @@ app.post("/api/atcoder/users", async (c) => {
     return c.text("Bad Request", 400);
   const userData = [];
   for (const user of users) {
+    const cache = await c.env.USER_CACHE.get(user);
+    if (cache) {
+      const { data, timestamp } = JSON.parse(cache);
+      if (Date.now() - timestamp < 1000 * 60 * 60) {
+        userData.push(data);
+        continue;
+      }
+    }
     const res = await fetch(`https://atcoder.jp/users/${user}/history/json`);
     const data = await res.json();
     userData.push(data);
+    await c.env.USER_CACHE.put(user, JSON.stringify({
+      data,
+      timestamp: Date.now(),
+    }));
     await new Promise((resolve) => setTimeout(resolve, 300));
   }
   return c.json(userData);
