7f0abf3982b5b0eeb4287acebcf07c9fb042e497
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-10-29 22:35:00 +0900

    月間と年間で絞り込めるようにする（バグあり）

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   // AtCoderのユーザー名
   const members = [
     "batora",
-    "a01sa01to",
+    "a01sa01to"
   ];
 
   // 表示するデータ
@@ -20,34 +20,66 @@ function App() {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const data = await response.json();
-      console.log(data);
       return data;
     } catch (error) {
       console.error("Fetch failed:", error);
     }
   };
-
+  
   // ページを読み込んだときにデータを取得して表示
   const getAtCoderData = async () => {
     const promises = members.map((member) => fetchData(member));
     const results = await Promise.all(promises);
     setData(results);
   };
+  
+  // comboboxの選択肢によってソートする
+  const handleChange = (e) => {
+    const value = e.target.value;
+    let sortedData = [];
+    if (value === "monthly") {
+      // d.EndTimeを参照して今月のデータを取得
+      sortedData = data.map((d) => d.filter((item) => {
+        const now = new Date();
+        const end = new Date(item.EndTime);
+        return now.getMonth() === end.getMonth() && now.getFullYear() === end.getFullYear();
+      }));
+    } else if (value === "yearly") {
+      // d.EndTimeを参照して年間のデータを取得
+      sortedData = data.map((d) => d.filter((item) => {
+        const now = new Date();
+        const end = new Date(item.EndTime);
+        return now.getFullYear() === end.getFullYear();
+      }));
+    } else if (value === "lifetime") {
+      getAtCoderData();
+      return;
+    }
+    console.log(sortedData);
+    setData(sortedData);
+  };
 
   // ページの読み込み時にgetAtCoderDataを実行
   useEffect(() => {
     getAtCoderData();
   }, []);
-
+  
   return (
     <div className="App">
       <h1>Maximum AtCoder Leaderboard</h1>
+      <select onChange={handleChange}>
+        <option value="lifetime">Lifetime</option>
+        <option value="monthly">Monthly</option>
+        <option value="yearly">Yearly</option>
+      </select>
+
       {data && data.map((d, index) => (
         <div key={index}>
           <h2>{members[index]}</h2>
           {d && d.map((item, index) => (
             <div key={index}>
-              {/* <p>終了時間: {item.EndTime}</p> */}
+              <p>{item.ContestName}</p>
+              <p>終了: {item.EndTime}</p>
               <p>増分: {item.NewRating - item.OldRating}</p>
             </div>
           ))}
